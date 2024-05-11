77ed443300e5bff52be2819c880314dd1281dfbf
Othmane <othmane.alaouimekkaoui@gmail.com>
2024-05-11 13:46:30 +0000

    update some files

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,20 +1,34 @@
--- script that creates a stored procedure ComputeAverageWeightedScoreForUser.
--- that computes and store the average weighted score for a student
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+-- script that creates a stored procedure ComputeAverageWeightedScoreForUser
+-- that computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE sum_weighted_scores FLOAT;
-    DECLARE total_weight FLOAT;
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
 
-    SELECT SUM(c.score * p.weight), SUM(p.weight)
-    INTO sum_weighted_scores, total_weight
-    FROM corrections c
-    JOIN projects p ON c.project_id = p.id
-    WHERE c.user_id = user_id;
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
 
-    DECLARE avg_weighted_score FLOAT;
-    SET avg_weighted_score = sum_weighted_scores / total_weight;
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
 
-    UPDATE users
-    SET average_score = avg_weighted_score
-    WHERE id = user_id;
-END //
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
+END $$
+DELIMITER ;
