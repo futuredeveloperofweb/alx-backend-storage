6758235ca910c3abc733071d6cbd8b65dad57618
Othmane <othmane.alaouimekkaoui@gmail.com>
2024-05-15 01:23:33 +0000

    tasks

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" module for task 8 """
+
+
+def list_all(mongo_collection):
+    """lists all documents in a collection"""
+    return [doc for doc in mongo_collection.find()]
